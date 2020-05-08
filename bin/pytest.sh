4b69683ad7e3df9b2f1d85c64f2e83a854db0a0d
Ilja Orlovs <vrghost@gmail.com>
2020-05-08 21:51:28 +0100

    unifying CI and pytest script

--- bin/pytest.sh
+++ bin/pytest.sh
@@ -4,9 +4,13 @@ TEST_ARGS="$@"
 
 source "$(dirname "${BASH_SOURCE[0]}")/env.sh"
 
-ENV_NAME='py-js-json-test'
-init_venv "${ENV_NAME}"
-source "$(get_venv_root "${ENV_NAME}")/bin/activate"
-maybe_run_pip_install "${ENV_NAME}" "${REQUIREMENTS_DIR}/development.txt"
+if [[ -z "${SKIP_VENV_INIT}" ]]
+then
+    ENV_NAME='py-js-json-test'
+    init_venv "${ENV_NAME}"
+    source "$(get_venv_root "${ENV_NAME}")/bin/activate"
+    maybe_run_pip_install "${ENV_NAME}" "${REQUIREMENTS_DIR}/development.txt"
+fi
 
+cd "${PROJ_ROOT}"
 exec python -m pytest -c "${PROJ_ROOT}/pytest.ini" "${TEST_ARGS[@]}"
