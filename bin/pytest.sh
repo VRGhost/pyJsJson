e4b6c057f34bdd362e8c2d76119010f22d264eb8
Ilja Orlovs <vrghost@gmail.com>
2020-05-08 16:11:18 +0100

    examples testing

--- bin/pytest.sh
+++ bin/pytest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+TEST_ARGS="$@"
+
 source "$(dirname "${BASH_SOURCE[0]}")/env.sh"
 
 ENV_NAME='py-js-json-test'
@@ -7,4 +9,4 @@ init_venv "${ENV_NAME}"
 source "$(get_venv_root "${ENV_NAME}")/bin/activate"
 maybe_run_pip_install "${ENV_NAME}" "${REQUIREMENTS_DIR}/development.txt"
 
-exec python -m pytest -c "${PROJ_ROOT}/pytest.ini"
+exec python -m pytest -c "${PROJ_ROOT}/pytest.ini" "${TEST_ARGS[@]}"
--- tests/cli/test_examples.py
+++ tests/cli/test_examples.py
@@ -1,5 +1,50 @@
 """This test uses 'examples' folder to test CLI behaviour."""
 
+import os
+import glob
+import subprocess
+import json
 
-def test_example():
-    1+1
+import pytest
+
+
+@pytest.fixture
+def EXAMPLES_DIR(PROJECT_ROOT):
+    return os.path.join(PROJECT_ROOT, 'examples')
+
+
+@pytest.mark.parametrize('update_path_by', [
+    'cwd', 'cli_arg'
+])
+def test_examples(cli_popen_args, EXAMPLES_DIR, update_path_by):
+    exampele_files = dict(
+        (
+            # fname -> full path
+            os.path.basename(full_path),
+            full_path
+        )
+        for full_path in glob.glob(os.path.join(EXAMPLES_DIR, '*.json'))
+    )
+
+    # Call the example via the CLI (but only if -output exists. Otherwise just fail.)
+    for fname in exampele_files.keys():
+        expected_out_fname = "{}-output.json".format(os.path.splitext(fname)[0])
+        if (expected_out_fname not in exampele_files):
+            assert fname.endswith('-output.json'), "Only output files are allowed not to have their outputs"
+            continue
+
+        call_args = cli_popen_args.copy()
+        call_args['args'] += (exampele_files[fname], )
+        if update_path_by == 'cwd':
+            call_args['cwd'] = EXAMPLES_DIR
+        elif update_path_by == 'cli_arg':
+            # cwd somewhere meaningless so the relative path lookups fail
+            call_args['cwd'] = os.path.abspath(os.sep) # This is FS root
+            call_args['args'] += ('--search-dirs', EXAMPLES_DIR)
+        else:
+            raise NotImplementedError(update_path_by)
+
+        out_json = subprocess.check_call(**call_args)
+        with open(exampele_files[expected_out_fname], 'r') as fin:
+            expected_out = json.load(fin)
+        assert out_json == expected_out
